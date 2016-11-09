119c79b707466750e0ffb165b6aefad27439e74f
kalekale <lehikoinen.kalle@gmail.com>
2016-11-09 18:31:00 +0200

    basic stuff

--- src/cljs/tsoha16/core.cljs
+++ src/cljs/tsoha16/core.cljs
@@ -19,22 +19,57 @@
  :initialize
  (fn
    [_ _]
-   {}))
+   {:aihealueet [{:nimi "Mukavat asiat" :id "1"} {:nimi "Jännät jutut" :id "2"} {:nimi "Niitä näitä" :id "3"}]
+    :keskustelut-aihealueittain {"1" [{:nimi "Hieno päivä tänään" :viesti "Hei kaikki! Tänään on todella hieno päivä. Join aamukahvit ja kävin pitkällä kävelyllä kauniissa talvisäässä." :id "1"}]}
+    }))
+
+;;=-------------
+;;SUBSRIPTIONSa
+
+(re-frame/register-sub
+ :aihealueet
+ (fn [db _]
+   (reaction (:aihealueet @db))))
+
+(re-frame/register-sub
+ :aiheet
+ (fn [db [_ aihealue-id]]
+   (reaction (get-in @db [:keskustelut-aihealueittain aihealue-id])))) 
 
 
 ;; -------------------------
 ;; Views
 
+(defn aihealue-sivu [{id :id nimi :nimi}]
+  (print id)
+  (let [aiheet (re-frame/subscribe [:aiheet id])]
+    [:div
+     [:h2 nimi]
+     [:ul
+      (for [aihe @aiheet]
+         ^{:key aihe} [:li (:nimi aihe)])]]))
+
+(defn aihealue-komponentti [aihealue]
+  [:li
+   [:a {:href (str "#/aihealueet/" (:id aihealue) "/" (:nimi aihealue))} (:nimi aihealue)]])
+
+(defn aihealue-lista []
+  (let [aihealueet (re-frame/subscribe [:aihealueet])]
+    (fn []
+      [:ul
+       (for [aihealue @aihealueet]
+         ^{:key aihealue} [aihealue-komponentti aihealue])])))
+
 (defn home-page []
-  [:div [:h2 "Hello world "]
-   [:div [:a {:href "/"} "about me"]]]) 
+  [:div [:h2 "Keskustelupalsta"]
+   [aihealue-lista]])
 
 (defn about-page []
   [:div [:h2 "About tsoha16"]
    [:div [:a {:href "/"} "go to the home page"]]]) 
 
 (defn current-page [] 
-  [:div [(session/get :current-page)]])
+  [:div [(session/get :current-page) (session/get :params)]])
 
 ;; -------------------------
 ;; Routes
@@ -42,7 +77,12 @@
 (secretary/defroute "/" []
   (session/put! :current-page #'home-page))
 
-(secretary/defroute "/about" []
+(secretary/defroute "/#/aihealueet/:id/:nimi" {:as params}
+  (session/put! :current-page #'aihealue-sivu)
+  (session/put! :params params))
+
+
+(secretary/defroute "/#/about" []
   (session/put! :current-page #'about-page))
  
 ;; ------------------------ 
