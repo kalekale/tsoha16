690a3b6c5f8a7d9661f3a0ff18ad2bec9b080727
kalekale <lehikoinen.kalle@gmail.com>
2017-03-30 23:16:19 +0300

    vikko 2 jutut

---
+++ sql/create_tables.sql
@@ -0,0 +1,26 @@
+CREATE TABLE Kayttaja(
+id SERIAL PRIMARY KEY,
+name varchar(50) NOT NULL,
+password varchar(50) NOT NULL,
+admin boolean NOT NULL
+);
+
+CREATE TABLE Aihe(
+id SERIAL PRIMARY KEY,
+kayttaja_id INTEGER REFERENCES Kayttaja(id),
+otsikko varchar(50) NOT NULL
+);
+
+CREATE TABLE Viesti(
+id SERIAL PRIMARY KEY,
+kayttaja_id INTEGER REFERENCES Kayttaja(id),
+aihe_id INTEGER REFERENCES Aihe(id),
+viesti varchar(10000) NOT NULL
+);
+
+CREATE TABLE Luettu(
+id SERIAL PRIMARY KEY,
+kayttaja_id INTEGER REFERENCES Kayttaja(id),
+aihe_id INTEGER REFERENCES Aihe(id),
+kuettu boolean NOT NULL
+);
